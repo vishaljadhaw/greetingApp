8d61d2d45b2e5450a6fe6f8b375686bf32609708
Vishal Jadhav <vishaljadhav56561@gmail.com>
2025-02-25 10:45:10 +0530

    Initialized greetingApp repository

---
+++ src/test/java/com/example/greetingApp/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
